f2d78626dd2995a0ae8463a52d5a3acfc5dfe6d5
bhvadeepreddy <bhavadeep2003@gmail.com>
2021-12-30 17:01:08 +0530

    changed increment count

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
+/* Place your JavaScript in this file */
 let count = 0
-
+const view = document.querySelector("#count")
 function increment()
 {
     count = count + 1
-    document.getElementById("count").innerText = count
+    view.innerText = count
+    setColor();
 }
 
 function decrement()
 {
     count = count - 1
-    document.getElementById("count").innerText = count
+    view.innerText = count
+    setColor();
 }
 function reset()
 {
     count = 0
-    document.getElementById("count").innerText = count
+    view.innerText = count
+    setColor();
+}
+
+function setColor(){
+    if(count>0){
+        view.style.color="green";
+    }
+    else if(count<0){
+        view.style.color = "red";
+    }
+    else{
+        view.style.color = "black";
+    }
 }
